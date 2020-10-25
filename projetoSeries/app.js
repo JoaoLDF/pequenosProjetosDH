cb6ebd64db4fae3d19b8b497a4912625f719a7ba
Joao Lourenco <joaofilhodias3@gmail.com>
2020-10-25 20:03:07 -0300

    mostra um array com a série referente ao id

--- projetoSeries/app.js
+++ projetoSeries/app.js
@@ -3,10 +3,9 @@ const app = express();
 
 let series = [{id:1,name:"Friends"},{id:2,name:"Breaking Bad"},{id:3,name:"Dexter" },{id:4,name:"Six Feet Under"}];
 
-app.get("/serie/:id?", (req, res)=> {
+app.get("/serie/:id?", (req, res)=>{
     let {id} = req.params;
-    let filtro = series.filter((item) =>item.id == id);
-    filtro.length>=1?filtro.forEach((item)=>res.send(item.name)):res.send("id não cadastrado");
+    res.send(series.filter((item) =>item.id == id));
 });
 
 app.listen(2000,()=>console.log('servidor rodando na porta 2000'));
