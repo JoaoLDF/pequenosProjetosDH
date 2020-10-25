52f9e79a76ea8f00ae10cf14d030aa2aa381a013
Joao Lourenco <joaofilhodias3@gmail.com>
2020-10-25 15:54:22 -0300

    Usando Método POST

--- projetoExpress/app.js
+++ projetoExpress/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 
-let app = express();
+const app = express();
 
+
+let produto = {
+    tipoProduto: null,
+    preco: null,
+    quantidade: null
+  }
+  
+app.post("/produto/criar", (req, res)=> res.send(produto = {tipoProduto:"living", preco:1245, quantidade:300}));
+app.get('/produtos/:id?', (req, res)=> {
+    let {id} = req.params;
+    console.log('estou no id: ', id);
+});
 app.get('/', (req,res)=>res.send('Olá Mundo!'));
 app.listen(3000,()=>console.log('servidor rodando na porta 3000'));
