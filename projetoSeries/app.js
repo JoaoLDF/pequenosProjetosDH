f44945e1b2e5946d486bf44e66082b875d036031
Joao Lourenco <joaofilhodias3@gmail.com>
2020-10-25 20:45:58 -0300

    Código aceito pelo playground

--- projetoSeries/app.js
+++ projetoSeries/app.js
@@ -3,9 +3,10 @@ const app = express();
 
 let series = [{id:1,name:"Friends"},{id:2,name:"Breaking Bad"},{id:3,name:"Dexter" },{id:4,name:"Six Feet Under"}];
 
-app.get("/serie/:id?", (req, res)=>{
+//Devolve a série de acordo com o id solicitado
+app.get("/serie/:id", (req, res)=>{
     let {id} = req.params;
-    res.send(series.filter((item) =>item.id == id));
+    res.send(series[id-1]);
 });
 
 app.listen(2000,()=>console.log('servidor rodando na porta 2000'));
