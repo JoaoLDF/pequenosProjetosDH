b632869efeb634e198832fd8ee8163cc7825f955
Joao Lourenco <joaofilhodias3@gmail.com>
2020-10-25 18:48:38 -0300

    segunda tentativa play ground

--- projetoSeries/app.js
+++ projetoSeries/app.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const app = express();
 
-let serie = [{id:1,name:"Friends"},{id:2,name:"Breaking Bad"},{id:3,name:"Dexter" },{id:4,name:"Six Feet Under"}];
-const seriePorId = (array, idReq) =>{
-    let resultado = array.filter((item) =>item.id == idReq);
+let series = [{id:1,name:"Friends"},{id:2,name:"Breaking Bad"},{id:3,name:"Dexter" },{id:4,name:"Six Feet Under"}];
 
-    if (resultado.length !== 0){
-        return resultado[0]
-    } else {
-        return "Esta página não existe";
-    }
-}
-
-app.get("/", (req, res)=> res.send("Você está no index"));
 app.get("/serie/:id?", (req, res)=> {
     let {id} = req.params;
-    console.log(id);
-    res.send(seriePorId(serie, id));
+    let filtro = series.filter((item) =>item.id == id);
+    filtro.length>=1?filtro.forEach((item)=>res.send(item)):res.send("id não cadastrado");
 });
 
 app.listen(2000,()=>console.log('servidor rodando na porta 2000'));
